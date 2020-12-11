579d011cb9238b365ce53624a72d0b0cc7eef615
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2020-12-11 22:19:36 +0700

    Create a database, change settings.py.
    create a barebone sqlite3 database and correct a mistake in settings.py (LANGUAGE_CODE and TIME_ZONE)

--- Web_Project/settings.py
+++ Web_Project/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'Asia/Jakarta'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 
 USE_I18N = True
 
