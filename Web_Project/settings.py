734565283a5c72be725240f8d730f29f88417d28
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2020-12-14 17:46:32 +0700

    Revert "Create table in database"
    This reverts commit b05b8ca1ac2ca99137ac86d8bb3ad72b4c570e51.

--- Web_Project/settings.py
+++ Web_Project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Jobsair_id',
+    'Jobsair_id.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
