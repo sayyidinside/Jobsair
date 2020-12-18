1d6bd149e65585c87e6b000ff1b7c46daddf56f0
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2020-12-18 18:57:46 +0700

    Fix for html in tamplate
    HTML file in tamplate now can access bootstrap, css, asset, etc

--- Jobsair_id/urls.py
+++ Jobsair_id/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 
 urlpatterns = [path('', views.post_list, name='post_list'), ]
+
+urlpatterns += staticfiles_urlpatterns()
