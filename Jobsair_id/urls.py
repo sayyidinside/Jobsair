aa2f2762d001a9287d4e8080a5c321ec10d2befd
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2020-12-18 19:43:19 +0700

    Fix Login.html and untrack/ignore cache in Web_Project
    fixing all issue in Login.html
    add Login.html to file to tamplate directroy for hosting purpose

--- Jobsair_id/urls.py
+++ Jobsair_id/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 
-urlpatterns = [path('', views.post_list, name='post_list'), ]
-
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns = [path('', views.post_list, name='post_list'),
+               path('login/', views.login_user, name='login'), ]
--- Jobsair_id/views.py
+++ Jobsair_id/views.py
@@ -7,3 +7,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'Jobsair_id/post_list.html', {'posts': posts})
+
+
+def login_user(request):
+    return render(request, 'Jobsair_id/Login.html', {})
