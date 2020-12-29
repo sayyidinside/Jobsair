8fa57f96d065bddb7b4331ba12be381e59dbb4c1
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2020-12-29 11:01:28 +0700

    Makeing forms look prettier using widget in meta

--- Jobsair_id/forms.py
+++ Jobsair_id/forms.py
@@ -8,3 +8,9 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'category', 'salary', 'job_desc', 'address',
                   'comp_link',)
+        widgets = {'title': forms.TextInput(attrs={'class': 'form-control'}), 
+                   'category': forms.Select(attrs={'class': 'form-control'}),
+                   'salary': forms.NumberInput(attrs={'class': 'form-control'}), 
+                   'job_desc': forms.Textarea(attrs={'class': 'form-control'}),
+                   'address': forms.Textarea(attrs={'class': 'form-control'}),
+                   'comp_link': forms.URLInput(attrs={'class': 'form-control'}),}
